9d6dbbadfea43ead0cd5339cfceefa09f95ca364
jgonyo-snag <jgonyo@snagajob.com>
2021-11-16 02:35:49 -0500

    fix

--- src/Parallafka/ParallafkaConfig.cs
+++ src/Parallafka/ParallafkaConfig.cs
@@ -5,12 +5,18 @@ namespace Parallafka
 {
     public class ParallafkaConfig<TKey, TValue> : IParallafkaConfig<TKey, TValue>
     {
+        /// <inheritdoc />
         public int MaxDegreeOfParallelism { get; set; } = 3;
 
+        /// <inheritdoc />
         public ILogger Logger { get; set; } =
             LoggerFactory.Create(builder => builder.AddConsole())
                 .CreateLogger<IParallafka<TKey, TValue>>();
 
+        /// <inheritdoc />
         public TimeSpan? CommitDelay { get; set; } = null;
+
+        /// <inheritdoc />
+        public int? MaxQueuedMessages { get; set; } = null;
     }
 }
